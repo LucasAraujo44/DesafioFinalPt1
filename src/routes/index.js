cdb77c4fcc6712e26e218761757acad33b8492dd
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-06-03 12:50:04 -0300

    Feat: Add router Person

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,14 @@
-const {Router} = require('express')
+const { Router } = require('express')
 const car = require('./car.router')
+const person = require('./person.router')
 
-module.exports = (server) =>{
-    server.use((req, res, next) =>{
+module.exports = (server) => {
+    server.use((req, res, next) => {
         car(server, new Router())
+        next()
+    })
+    server.use((req, res, next) => {
+        person(server, new Router())
         next()
     })
 }
---
+++ src/routes/person.router.js
@@ -0,0 +1,7 @@
+const PersonController = require("../app/controller/PersonController")
+
+module.exports = (server, routes, prefix = "/api/v1/person") => {
+  routes.post("/", PersonController.create)
+  server.use(prefix, routes)
+
+}
