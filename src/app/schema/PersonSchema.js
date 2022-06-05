6a9c2de56f76aea3e69fbed27c53965edaeb69c0
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-06-05 14:56:26 -0300

    Feat: Add encoding password for PersonSchema

--- src/app/schema/PersonSchema.js
+++ src/app/schema/PersonSchema.js
@@ -1,37 +1,47 @@
 const mongoose = require('mongoose')
-const mongoosePaginte = require('mongoose-paginate-v2')
+const mongoosePaginate = require('mongoose-paginate-v2')
+const bcrypt = require ('bcryptjs')
+
 
 const PersonSchema = new mongoose.Schema(
     {
-    name: {
-        type: String,
-        required: true
-    },
-    cpf: {
-        type: String,
-        required: true
-    },
-    birthDay:{
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    canDrive: {
-        type: String,
-        required: true,
-        enum: {
-            values: ["yes", "no"],
-            message: "Value is not supported"
+        name: {
+            type: String,
+            required: true
+        },
+        cpf: {
+            type: String,
+            required: true
+        },
+        birthDay: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true,
+            select: false,
+        },
+        canDrive: {
+            type: String,
+            required: true,
+            enum: {
+                values: ["yes", "no"],
+                message: "Value is not supported"
+            }
         }
-    }
-})
-PersonSchema.plugin(mongoosePaginte)
-const Person = mongoose.model("Person",PersonSchema)
+    })
+    PersonSchema.pre('save', async function (next) {
+        const hash = await bcrypt.hash(this.password, 10)
+        this.password = hash
+        next()
+    }) 
+PersonSchema.plugin(mongoosePaginate)
+const Person = mongoose.model("Person", PersonSchema)
+
+
 module.exports = Person
