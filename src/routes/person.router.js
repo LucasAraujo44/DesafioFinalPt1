58e00c17fd8ca22ebfb6758c88ca200fb759623a
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-06-06 09:02:32 -0300

    Feat: Add validation in endpoint PUT of person

--- src/routes/person.router.js
+++ src/routes/person.router.js
@@ -4,7 +4,7 @@ const PersonValidation = require('../app/validation/Person/PersonValidation')
 module.exports = (server, routes, prefix = "/api/v1/person") => {
   routes.post("/",PersonValidation, PersonController.create)
   routes.get("/", PersonController.list)
-  routes.put("/:id", PersonController.update)
+  routes.put("/:id",PersonValidation, PersonController.update)
   routes.get("/:id", PersonController.getById)
   routes.delete("/:id", PersonController.delete)
 
