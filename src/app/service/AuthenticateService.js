490c6c9d43ac7ce7cfba94720b29383f9aa4523e
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-06-07 23:02:04 -0300

    Feat: Add token JTW and config

--- src/app/service/AuthenticateService.js
+++ src/app/service/AuthenticateService.js
@@ -1,23 +1,29 @@
 const PersonRepository = require("../repository/PersonRepository")
 const bcrypt = require('bcryptjs')
+const jwt = require ('jsonwebtoken')
+const authConfig = require('../../config/auth.json')
 const Person = require("../schema/PersonSchema")
 
 class AuthenticateService {
     //Create
-    async create(email, password) {
+     async create(email, password) {
         const result = await PersonRepository.findOneEmail(email)
         if (!result) {
-            throw new Error('Bad required')
+            throw new Error('Person not found')
         }
         const test = await bcrypt.compare(password, result.password)
         if (!test) {
 
-            throw new Error('Bad required')
+            throw new Error('Invalid password')
 
         }
         result.password = undefined
-        return result
-    }
-   
+        const token = jwt.sign({ _id: Person.id}, authConfig.secret,{
+            expiresIn: 86400
+        } )
+         return ({result, token}) 
+
+    } 
+
 }
 module.exports = new AuthenticateService()
