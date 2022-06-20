540aa65f6dc793603466c500d3b6889bcce707b6
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-06-20 16:39:52 -0300

    Feat: add as rotas de Fleet in rental

--- src/routes/rental.router.js
+++ src/routes/rental.router.js
@@ -1,4 +1,5 @@
 const RentalController = require('../app/controller/RentalController')
+const FleetController = require('../app/controller/FleetController')
 
 module.exports = (server, routes, prefix = "/api/v1/rental") => {
     routes.post("/", RentalController.create)
@@ -6,6 +7,15 @@ module.exports = (server, routes, prefix = "/api/v1/rental") => {
     routes.put("/:id",RentalController.update)
     routes.get("/:id", RentalController.getById)
     routes.delete("/:id", RentalController.delete)
+    routes.post("/:id/fleet", FleetController.create)
+    routes.get("/:id/fleet", FleetController.listFleet)
+    routes.put("/:id/fleet/:id", FleetController.updateFleet)
+    routes.get("/:id/fleet/:id", FleetController.getByIdFleet)
+    routes.delete("/:id/fleet/:id", FleetController.deleteFleet)
+
+
+
+
 
 
 
