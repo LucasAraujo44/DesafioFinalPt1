dd6ffb5fa562b9bf2e1449e20611e59c3dc55553
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-06-21 16:27:23 -0300

    Feat: add CRUD of Reserve

---
+++ src/app/repository/ReserveRepository.js
@@ -0,0 +1,37 @@
+const ReserveSchema = require ('../schema/ReserveSchema')
+
+class ReserveRepository {
+    async createReserve(id_rental, body){
+        return await ReserveSchema.create(id_rental, body)
+    }
+    async listReserve(payload) {
+        const paginateFields = {
+          totalDocs: "total",
+          docs: "Reserves",
+          page: "offsets",
+          nextPage: false,
+          prevPage: false,
+          pagingCounter: "false",
+          meta: false,
+          hasPrevPage: false,
+          hasNextPage: false,
+          limit: "limit"
+        }
+        const options = {
+          page: 0,
+          limit: 20,
+          customLabels: paginateFields,
+        }
+        return ReserveSchema.paginate(payload, options, {})
+      }
+      async updateReserve(id, body) {
+        return ReserveSchema.findByIdAndUpdate(id, body)
+      }
+      async getByIdReserve(payload) {
+        return ReserveSchema.findById(payload)
+    }
+    async deleteReserve(payload) {
+        return ReserveSchema.findByIdAndDelete(payload);
+      }
+}
+module.exports = new ReserveRepository()
