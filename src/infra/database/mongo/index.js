238f31ef8bce090f8c00fb10ef1bcd81fe515ec3
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-06-21 23:16:56 -0300

    Feat: add connection mongo DB Atlas

--- src/infra/database/mongo/index.js
+++ src/infra/database/mongo/index.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 const config = require('../../../config/config')
+require('dotenv').config()
 
 class Database{
     connect(){
         return mongoose.connect
-        (`mongodb://${config.database.host}/${config.database.collection}`,
+        (process.env.DB_MONGO,
         {
             user: config.database.username,
             pass: config.database.password
