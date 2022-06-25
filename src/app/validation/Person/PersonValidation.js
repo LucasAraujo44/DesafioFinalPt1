e4f3e5277bd3aa9546e5623f8edab3a665fb09ba
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-06-25 10:25:52 -0300

    Feat: add file enum in person validation

--- src/app/validation/Person/PersonValidation.js
+++ src/app/validation/Person/PersonValidation.js
@@ -1,6 +1,8 @@
 const JoiDate = require('@joi/date')
 const Joi = require('joi').extend(JoiDate)
 const validateCpf = require('./CpfValidation')
+const {canDrive} = require('../../Enum/enums')
+const {email} = require('../../Enum/enums')
 
 module.exports = async (req, res, next) => {
     try {
@@ -9,25 +11,25 @@ module.exports = async (req, res, next) => {
             name: Joi.string().trim().required(),
             cpf: Joi.string().max(11).required(),
             birthDay: Joi.date().format('DD/MM/YYYY').required(),
-            email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'br'] } }).required(),
+            email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: [...email] } }).required(),
             password: Joi.string().alphanum().min(6).max(12).required(),
-            canDrive: Joi.string().valid("yes", "no").required()
+            canDrive: Joi.string().valid(...canDrive).required()
         })
         const { error } = await schema.validate(req.body, { abortEarly: true })
         if (error) {
             throw {
                 message: 'Bad Request',
-                details: [ {
-                  message : error.message
-                } ]
-              };
-            }
+                details: [{
+                    message: error.message
+                }]
+            };
+        }
         if (!validateCpf(req.body.cpf)) {
             throw {
                 message: 'Invalid CPF'
             }
         }
-        
+
         return next()
     } catch (error) {
         return res.status(400).json(error)
