392a72e579d18f190c8ea7a84e0c575c6fe39bb6
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-06-25 18:24:25 -0300

    Fix: status code error and success

--- src/app/controller/AuthenticateController.js
+++ src/app/controller/AuthenticateController.js
@@ -7,7 +7,7 @@ class AuthenticateController {
             const result = await AuthenticateService.create(email, password)
             return res.status(200).json(result)
         } catch (error) {
-            return res.status(400).json(error.message)
+            return res.status(401).json(error.message)
         }
     }
 }
