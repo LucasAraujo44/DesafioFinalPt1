befc7b7b7c31948786a524ddf632faa913e02657
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-06-25 11:09:02 -0300

    Feat: add rentalValidation in routes rental

--- src/routes/rental.router.js
+++ src/routes/rental.router.js
@@ -1,9 +1,9 @@
 const RentalController = require('../app/controller/RentalController')
-
+const RentalValidation = require('../app/validation/Rental/RentalValidation')
 module.exports = (server, routes, prefix = "/api/v1/rental") => {
-    routes.post("/", RentalController.create)
+    routes.post("/",RentalValidation, RentalController.create)
     routes.get("/", RentalController.list)
-    routes.put("/:id",RentalController.update)
+    routes.put("/:id",RentalValidation,RentalController.update)
     routes.get("/:id", RentalController.getById)
     routes.delete("/:id", RentalController.delete)
 
