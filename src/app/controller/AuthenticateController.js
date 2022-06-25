3d1f306853d1f2194301336b412e986eed38510c
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-06-25 11:11:18 -0300

    Fix: status code '201' for '200'

--- src/app/controller/AuthenticateController.js
+++ src/app/controller/AuthenticateController.js
@@ -5,7 +5,7 @@ class AuthenticateController {
         try {
             const {email,password} = req.body
             const result = await AuthenticateService.create(email, password)
-            return res.status(201).json(result)
+            return res.status(200).json(result)
         } catch (error) {
             return res.status(400).json(error.message)
         }
