10af40ede015418f3ac8911f1b15fb55478af7fb
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-06-25 10:23:50 -0300

    Feat: add file enums for reuse in other files

---
+++ src/app/Enum/enums.js
@@ -0,0 +1,7 @@
+const enums = {
+    canDrive: ['yes', 'no'],
+    status: ['available', 'unavailable','rented'],
+    email: ['com', 'net', 'br'],
+    isFilial: ['true', 'false']
+}
+module.exports = enums
